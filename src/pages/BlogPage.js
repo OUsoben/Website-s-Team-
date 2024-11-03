266fee89a5d1549e5e761d0839275341c57eeffc
OUsoben <ousoben20@gmail.com>
2024-11-03 22:32:10 +0700

    Chnage the blogs's page

--- src/pages/BlogPage.js
+++ src/pages/BlogPage.js
@@ -5,7 +5,9 @@ import "../styles/BlogStyle.css"
 const BlogPage = () => {
   return (
     <>
-        <h1> Blog Page</h1>
+    
+        <button className='btn btn-danger'>Sign up</button>
+        <p className='text-warning border-primary'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam aperiam assumenda suscipit facilis dolore! Dicta ullam saepe ab id ea aperiam expedita, vitae eos, doloribus quasi facilis quia illum ratione?</p>
     </>
   )
 }
