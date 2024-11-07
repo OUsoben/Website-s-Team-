6833ac8f224eefe2c4bb801622575af7d8a0206e
Panha <turtoisecool@gmail.com>
2024-11-07 12:37:00 +0700

    Done the first section (2 cards) of the blog page

--- src/pages/BlogPage.js
+++ src/pages/BlogPage.js
@@ -8,16 +8,40 @@ const BlogPage = () => {
 
   let navigate = useNavigate()
   return (
-  
     <>
-        <section style={{fontSize:"15px",marginLeft:"12px"}} className=' d-flex p-md-5 pt-2 '>
-         
-         <p style={{cursor:"pointer"}} className='text-muted ps-xl-5 font-style hover-home'  
-           onClick={()=>{
-             // Navigate to Home page
-              navigate("/")
-           }}
-         >Home</p> <p><FontAwesomeIcon style={{width:"30px"}} className=' font-style text-muted'  icon={faAngleRight} /> Blogs</p>
+      <section style={{fontSize:"15px",marginLeft:"12px"}} className=' d-flex p-md-5 pt-2 '>
+        
+        <p style={{cursor:"pointer"}} className='text-muted ps-xl-5 font-style hover-home'  
+          onClick={()=>{
+            // Navigate to Home page
+            navigate("/")
+          }}
+        >Home</p> <p><FontAwesomeIcon style={{width:"30px"}} className=' font-style text-muted'  icon={faAngleRight} /> Blogs</p>
+      </section>
+      <section>
+        <div style={{border:"2px solid red", marginLeft: "7.5vw", marginRight: "7.5vw"}}>
+          <div style={{border:"2px solid blue"}}>
+            <button type="button" class="btn btn-outline-primary" style={{marginRight: "15px"}}>Design</button>
+            <button type="button" class="btn btn-outline-primary" style={{marginRight: "15px"}}>Marketing</button>
+            <button type="button" class="btn btn-outline-primary" style={{marginRight: "15px"}}>Sell Online</button>
+            <button type="button" class="btn btn-outline-primary" style={{marginRight: "15px"}}>Payments</button>
+          </div>
+          <div className='d-flex flex-row mb-2'>
+            {/* Left card */}
+            <div className='d-flex flex-column mb-3 p-2'>
+              <img class="img-fluid" src="https://woocommerce.com/wp-content/uploads/2024/09/blog-hero-Business-Models@2x.jpg" alt="Woman, am I right?" />
+              <div className='p-2'>caption</div>
+              <div className='p-2'>date</div>
+              <div className='p-2'>category</div>
+            </div>
+            <div className='d-flex flex-column mb-3 p-2'>
+              <img class="img-fluid" src="https://woocommerce.com/wp-content/uploads/2024/09/blog-hero-Business-Models@2x.jpg" alt="Woman, am I right?" />
+              <div className='p-2'>caption</div>
+              <div className='p-2'>date</div>
+              <div className='p-2'>category</div>
+            </div>
+          </div>
+        </div>
       </section>
     </>
   )
