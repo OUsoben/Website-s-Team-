4bc04787be2e9bb7e70e7e21645f9e7992140a90
Panha <turtoisecool@gmail.com>
2024-11-08 18:47:27 +0700

    lol

--- src/pages/BlogPage.js
+++ src/pages/BlogPage.js
@@ -7,12 +7,7 @@ import { faAngleRight } from '@fortawesome/free-solid-svg-icons'
 const BlogPage = () => {
   let navigate = useNavigate()
   return (
-<<<<<<< HEAD
-    <div>
-
-=======
     <>
->>>>>>> b3adba9c08f665e73e390d6c28e27fa56018e181
       <section style={{fontSize:"15px",marginLeft:"12px"}} className=' d-flex p-md-5 pt-2 '>
         
         <p style={{cursor:"pointer"}} className='text-muted ps-xl-5 font-style hover-home'  
@@ -46,23 +41,12 @@ const BlogPage = () => {
             </div>
           </div>
         </div>
-<<<<<<< HEAD
-
-      </section>
-    
-        <button className='btn btn-danger'>Sign up</button>
-        <p className='text-warning border-primary'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam aperiam assumenda suscipit facilis dolore! Dicta ullam saepe ab id ea aperiam expedita, vitae eos, doloribus quasi facilis quia illum ratione?</p>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae, dolore praesentium. Asperiores ipsa inventore architecto voluptatibus in excepturi esse eum. Blanditiis at dolor nulla sint recusandae nostrum adipisci culpa quod.</p>
-     
-    </div>
-=======
       </section>
         <button className='btn btn-danger'>Sign up</button>
         <p className='text-warning border-primary'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam aperiam assumenda suscipit facilis dolore! Dicta ullam saepe ab id ea aperiam expedita, vitae eos, doloribus quasi facilis quia illum ratione?</p>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae, dolore praesentium. Asperiores ipsa inventore architecto voluptatibus in excepturi esse eum. Blanditiis at dolor nulla sint recusandae nostrum adipisci culpa quod.</p>
         origin/team
     </>
->>>>>>> b3adba9c08f665e73e390d6c28e27fa56018e181
   )
 }
 
